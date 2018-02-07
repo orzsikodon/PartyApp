5983641bdcd6d17dada6727862c5919632e81ec3
orzsikodon <odon.orzsik.17@dartmouth.edu>
2018-02-07 21:10:56 +0100

    authentication works

---
+++ app/src/main/java/com/example/android/partyappfox/SignInActivity.java
@@ -0,0 +1,153 @@
+package com.example.android.partyappfox;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText mEmail;
+    private EditText mPassword;
+    private TextView mPrompt;
+    private ProgressBar mProgBar;
+    private FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        // get the edit texts and text prompt
+        mEmail = (EditText) findViewById(R.id.email);
+        mPassword = (EditText) findViewById(R.id.password);
+        mPrompt = (TextView) findViewById(R.id.sign_in_prompt);
+        mProgBar = (ProgressBar) findViewById(R.id.progress_bar);
+
+        // set the onclick listeners
+        findViewById(R.id.email_sign_in_button).setOnClickListener(this);
+        findViewById(R.id.email_create_account_button).setOnClickListener(this);
+
+        // auth object initialization
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    /*
+    Very rudimentary check to validate the input into the email and password fields
+    At this stage it only checks if the input is empty
+     */
+    private boolean validateInput(){
+        boolean res = true;
+        String password = mPassword.getText().toString();
+        String email = mEmail.getText().toString();
+        // set the error based on what's missing
+        if (password.length() == 0 && email.length() == 0){
+            mPrompt.setTextColor(getResources().getColor(R.color.errorColor));
+            mPrompt.setText("Input an email and password!");
+            res = false;
+        }
+        else if (password.length() == 0){
+            mPrompt.setTextColor(getResources().getColor(R.color.errorColor));
+            mPrompt.setText("Input a password!");
+            res = false;
+        }
+        else if (email.length() == 0){
+            mPrompt.setTextColor(getResources().getColor(R.color.errorColor));
+            mPrompt.setText("Input an email!");
+            res = false;
+        }else {
+            mPrompt.setTextColor(getResources().getColor(R.color.textColor));
+            mPrompt.setText("Sign In or Create Account");
+            res = true;
+        }
+        return res;
+    }
+
+    private void showDialog(){
+        mProgBar.setVisibility(View.VISIBLE);
+    }
+
+    private void hideDialog(){
+        mProgBar.setVisibility(View.INVISIBLE);
+    }
+
+    // handle the button clicks
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+        if (id == R.id.email_sign_in_button){
+            signIn(mEmail.getText().toString(), mPassword.getText().toString());
+        }
+        else if (id == R.id.email_create_account_button){
+            createAccount(mEmail.getText().toString(), mPassword.getText().toString());
+        }
+    }
+
+    // create the new account
+    private void createAccount(String email, String password) {
+        // do not create an account if email or password is missing
+        if (!validateInput()) {
+            return;
+        }
+
+        showDialog();
+
+        // register the new user with Firebase
+        mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                // take the user to the main activity if the sign up was successful
+                if(task.isSuccessful()){
+                    Toast.makeText(SignInActivity.this, "Account created!", Toast.LENGTH_SHORT).show();
+                    hideDialog();
+                    Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+                }else {
+                    Toast.makeText(SignInActivity.this, "Account creation failed!", Toast.LENGTH_SHORT).show();
+                    hideDialog();
+                }
+            }
+        });
+    }
+
+    // sign in user
+    private void signIn(String email, String password) {
+        // do not create an account if email or password is missing
+        if (!validateInput()) {
+            return;
+        }
+
+        showDialog();
+
+        // sign in an already existing user
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                // take the user to the main activity if the sign in was successful
+                if(task.isSuccessful()){
+                    Toast.makeText(SignInActivity.this, "Signed in!", Toast.LENGTH_SHORT).show();
+                    hideDialog();
+                    Intent mainIntent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+                }else {
+                    Toast.makeText(SignInActivity.this, "Account creation failed!", Toast.LENGTH_SHORT).show();
+                    hideDialog();
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/android/partyappfox/StartActivity.java
@@ -0,0 +1,42 @@
+package com.example.android.partyappfox;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+/**
+ * Created by odonorzsik on 2/6/18.
+ */
+
+public class StartActivity extends Activity {
+    // Firebase instance variables
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Initialize Firebase Authentication
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+
+        if (mFirebaseUser == null){
+            // if user is not logged in take him to sign in screen
+            Intent mSignInIntent = new Intent(this, SignInActivity.class);
+            startActivity(mSignInIntent);
+            finish();
+        }
+        else {
+            // user is logged in take him to main activity
+            Intent mSignInIntent = new Intent(this, MainActivity.class);
+            startActivity(mSignInIntent);
+            finish();
+        }
+    }
+}
